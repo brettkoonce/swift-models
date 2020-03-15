226102ac5cbdbad065be1951452348073ae418e8
brett koonce <koonce@gmail.com>
2020-03-15 11:41:47 -0500

    add resize utilities, other efficientnet variants

--- Models/ImageClassification/EfficientNet.swift
+++ Models/ImageClassification/EfficientNet.swift
@@ -18,7 +18,37 @@ import TensorFlow
 // "EfficientNet: Rethinking Model Scaling for Convolutional Neural Networks"
 // Mingxing Tan, Quoc V. Le
 // https://arxiv.org/abs/1905.11946
-// Notes: Baseline (B0) network, table 1
+// Notes: Default baseline (B0) network, see table 1
+
+/// some utility functions to help generate network variants
+/// original: https://github.com/tensorflow/tpu/blob/d6f2ef3edfeb4b1c2039b81014dc5271a7753832/models/official/efficientnet/efficientnet_model.py#L138
+fileprivate var depthCoefficient: Float = 1.0
+fileprivate func roundBlockDepthUp(blockCount: Int) -> Int {
+    /// Multiply + round up the number of blocks based on global depth multiplier
+    var newFilterCount = depthCoefficient * Float(blockCount)
+    newFilterCount.round(.up)
+    print (blockCount, newFilterCount)
+    return Int(newFilterCount)
+}
+
+fileprivate var widthCoefficient: Float = 1.0
+fileprivate func roundFilterCountDown(filter: Int, depthDivisor: Float = 8.0) -> Int {
+    /// Multiply + round down the number of filters based on global width multiplier
+    let filterMult = Float(filter) * widthCoefficient
+    let filterAdd = Float(filterMult) + (depthDivisor / 2.0)
+    var div = filterAdd / depthDivisor
+    div.round(.down)
+    div = div * Float(depthDivisor)
+    var newFilterCount = max(1, Int(div))
+    if newFilterCount < Int(0.9 * Float(filter)) {
+        newFilterCount += Int(depthDivisor)
+    }
+    return Int(newFilterCount)
+}
+
+fileprivate func roundFilterPair(filters: (Int, Int)) -> (Int, Int) {
+    return (roundFilterCountDown(filter: filters.0), roundFilterCountDown(filter: filters.1))
+}
 
 public struct InitialMBConvBlock: Layer {
     public var dConv: DepthwiseConv2D<Float>
@@ -29,24 +59,25 @@ public struct InitialMBConvBlock: Layer {
     public var batchNormConv: BatchNorm<Float>
 
     public init(filters: (Int, Int)) {
+        let filterMult = roundFilterPair(filters: filters)
         dConv = DepthwiseConv2D<Float>(
-            filterShape: (3, 3, filters.0, 1),
+            filterShape: (3, 3, filterMult.0, 1),
             strides: (1, 1),
             padding: .same)
         seReduceConv = Conv2D<Float>(
-            filterShape: (1, 1, filters.0, 8),
+            filterShape: (1, 1, filterMult.0, roundFilterCountDown(filter: 8)),
             strides: (1, 1),
             padding: .same)
         seExpandConv = Conv2D<Float>(
-            filterShape: (1, 1, 8, filters.0),
+            filterShape: (1, 1, roundFilterCountDown(filter: 8), filterMult.0),
             strides: (1, 1),
             padding: .same)
         conv2 = Conv2D<Float>(
-            filterShape: (1, 1, filters.0, filters.1),
+            filterShape: (1, 1, filterMult.0, filterMult.1),
             strides: (1, 1),
             padding: .same)
-        batchNormDConv = BatchNorm(featureCount: filters.0)
-        batchNormConv = BatchNorm(featureCount: filters.1)
+        batchNormDConv = BatchNorm(featureCount: filterMult.0)
+        batchNormConv = BatchNorm(featureCount: filterMult.1)
     }
 
     @differentiable
@@ -80,10 +111,11 @@ public struct MBConvBlock: Layer {
         self.strides = strides
         self.addResLayer = filters.0 == filters.1 && strides == (1, 1)
 
-        let hiddenDimension = filters.0 * depthMultiplier
-        let reducedDimension = max(1, Int(filters.0 / 4))
+        let filterMult = roundFilterPair(filters: filters)
+        let hiddenDimension = filterMult.0 * depthMultiplier
+        let reducedDimension = max(1, Int(filterMult.0 / 4))
         conv1 = Conv2D<Float>(
-            filterShape: (1, 1, filters.0, hiddenDimension),
+            filterShape: (1, 1, filterMult.0, hiddenDimension),
             strides: (1, 1),
             padding: .same)
         dConv = DepthwiseConv2D<Float>(
@@ -99,12 +131,12 @@ public struct MBConvBlock: Layer {
             strides: (1, 1),
             padding: .same)
         conv2 = Conv2D<Float>(
-            filterShape: (1, 1, hiddenDimension, filters.1),
+            filterShape: (1, 1, hiddenDimension, filterMult.1),
             strides: (1, 1),
             padding: .same)
         batchNormConv1 = BatchNorm(featureCount: hiddenDimension)
         batchNormDConv = BatchNorm(featureCount: hiddenDimension)
-        batchNormConv2 = BatchNorm(featureCount: filters.1)
+        batchNormConv2 = BatchNorm(featureCount: filterMult.1)
     }
 
     @differentiable
@@ -136,9 +168,10 @@ public struct MBConvBlockStack: Layer {
         kernel: (Int, Int) = (3, 3),
         blockCount: Int
     ) {
+        let blockMult = roundBlockDepthUp(blockCount: blockCount)
         self.blocks = [MBConvBlock(filters: (filters.0, filters.1),
             strides: initialStrides, kernel: kernel)]
-        for _ in 1..<blockCount {
+        for _ in 1..<blockMult {
             self.blocks.append(MBConvBlock(filters: (filters.1, filters.1), kernel: kernel))
         }
     }
@@ -152,36 +185,59 @@ public struct MBConvBlockStack: Layer {
 public struct EfficientNet: Layer {
     public var inputConv: Conv2D<Float>
     public var inputConvBatchNorm: BatchNorm<Float>
-    public var initialMBConv = InitialMBConvBlock(filters: (32, 16))
+    public var initialMBConv: InitialMBConvBlock
 
-    public var residualBlockStack1 = MBConvBlockStack(filters: (16, 24), blockCount: 2)
-    public var residualBlockStack2 = MBConvBlockStack(filters: (24, 40), kernel: (5, 5),
-        blockCount: 2)
-    public var residualBlockStack3 = MBConvBlockStack(filters: (40, 80), blockCount: 3)
-    public var residualBlockStack4 = MBConvBlockStack(filters: (80, 112), initialStrides: (1, 1),
-        kernel: (5, 5), blockCount: 3)
-    public var residualBlockStack5 = MBConvBlockStack(filters: (112, 192), kernel: (5, 5),
-        blockCount: 4)
-    public var residualBlockStack6 = MBConvBlockStack(filters: (192, 320), initialStrides: (1, 1),
-        blockCount: 1)
+    public var residualBlockStack1: MBConvBlockStack
+    public var residualBlockStack2: MBConvBlockStack
+    public var residualBlockStack3: MBConvBlockStack
+    public var residualBlockStack4: MBConvBlockStack
+    public var residualBlockStack5: MBConvBlockStack
+    public var residualBlockStack6: MBConvBlockStack
 
     public var finalConv: Conv2D<Float>
     public var avgPool = GlobalAvgPool2D<Float>()
-    public var dropout = Dropout<Float>(probability: 0.2)
+    public var dropoutProb: Dropout<Float>
     public var output: Dense<Float>
 
-    public init(classCount: Int = 1000) {
+    /// default settings are efficientnetB0 (baseline) network
+    /// resolution is here to show what the network can take as input, it doesn't set anything!
+    public init(
+        classCount: Int = 1000,
+        width: Float = 1.0,
+        depth: Float = 1.0,
+        resolution: Int = 224,
+        dropout: Double = 0.2
+    ) {
+        depthCoefficient = depth
+        widthCoefficient = width
+
         inputConv = Conv2D<Float>(
-            filterShape: (3, 3, 3, 32),
+            filterShape: (3, 3, 3, roundFilterCountDown(filter: 32)),
             strides: (2, 2),
             padding: .same)
-        inputConvBatchNorm = BatchNorm(featureCount: 32)
+        inputConvBatchNorm = BatchNorm(featureCount: roundFilterCountDown(filter: 32))
+
+        // global filter resizing happens at block layer
+        initialMBConv = InitialMBConvBlock(filters: (32, 16))
+
+        // global block resizing happens at stack layer
+        residualBlockStack1 = MBConvBlockStack(filters: (16, 24), blockCount: 2)
+        residualBlockStack2 = MBConvBlockStack(filters: (24, 40), kernel: (5, 5),
+            blockCount: 2)
+        residualBlockStack3 = MBConvBlockStack(filters: (40, 80), blockCount: 3)
+        residualBlockStack4 = MBConvBlockStack(filters: (80, 112), initialStrides: (1, 1),
+            kernel: (5, 5), blockCount: 3)
+        residualBlockStack5 = MBConvBlockStack(filters: (112, 192), kernel: (5, 5),
+            blockCount: 4)
+        residualBlockStack6 = MBConvBlockStack(filters: (192, 320), initialStrides: (1, 1),
+            blockCount: 1)
 
         finalConv = Conv2D<Float>(
-            filterShape: (1, 1, 320, 1280),
+            filterShape: (1, 1, roundFilterCountDown(filter: 320), roundFilterCountDown(filter: 1280)),
             strides: (1, 1),
             padding: .same)
-        output = Dense(inputSize: 1280, outputSize: classCount)
+        dropoutProb = Dropout<Float>(probability: dropout)
+        output = Dense(inputSize: roundFilterCountDown(filter: 1280), outputSize: classCount)
     }
 
     @differentiable
@@ -190,6 +246,43 @@ public struct EfficientNet: Layer {
             initialMBConv)
         let backbone = convolved.sequenced(through: residualBlockStack1, residualBlockStack2,
             residualBlockStack3, residualBlockStack4, residualBlockStack5, residualBlockStack6)
-        return backbone.sequenced(through: finalConv, avgPool, dropout, output)
+        return backbone.sequenced(through: finalConv, avgPool, dropoutProb, output)
+    }
+}
+
+extension EfficientNet {
+    public enum Kind {
+        case efficientnetB0
+        case efficientnetB1
+        case efficientnetB2
+        case efficientnetB3
+        case efficientnetB4
+        case efficientnetB5
+        case efficientnetB6
+        case efficientnetB7
+        case efficientnetB8
+    }
+
+    public init(kind: Kind, classCount: Int = 1000) {
+        switch kind {
+        case .efficientnetB0:
+            self.init(classCount: classCount, width: 1.0, depth: 1.0, resolution: 224, dropout: 0.2)
+        case .efficientnetB1:
+            self.init(classCount: classCount, width: 1.0, depth: 1.1, resolution: 240, dropout: 0.2)
+        case .efficientnetB2:
+            self.init(classCount: classCount, width: 1.1, depth: 1.2, resolution: 260, dropout: 0.3)
+        case .efficientnetB3:
+            self.init(classCount: classCount, width: 1.2, depth: 1.4, resolution: 300, dropout: 0.3)
+        case .efficientnetB4:
+            self.init(classCount: classCount, width: 1.4, depth: 1.8, resolution: 380, dropout: 0.4)
+        case .efficientnetB5:
+            self.init(classCount: classCount, width: 1.6, depth: 2.2, resolution: 456, dropout: 0.4)
+        case .efficientnetB6:
+            self.init(classCount: classCount, width: 1.8, depth: 2.6, resolution: 528, dropout: 0.5)
+        case .efficientnetB7:
+            self.init(classCount: classCount, width: 2.0, depth: 3.1, resolution: 600, dropout: 0.5)
+        case .efficientnetB8:
+            self.init(classCount: classCount, width: 2.2, depth: 3.6, resolution: 672, dropout: 0.5)
+        }
     }
 }
