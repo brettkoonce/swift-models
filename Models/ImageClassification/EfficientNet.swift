cfd3ebea9ae71d273cf8f923acf6f12fa6330f33
brett koonce <koonce@gmail.com>
2020-03-26 19:12:56 -0500

    rebuild filter resize logic

--- Models/ImageClassification/EfficientNet.swift
+++ Models/ImageClassification/EfficientNet.swift
@@ -22,31 +22,30 @@ import TensorFlow
 
 /// some utility functions to help generate network variants
 /// original: https://github.com/tensorflow/tpu/blob/d6f2ef3edfeb4b1c2039b81014dc5271a7753832/models/official/efficientnet/efficientnet_model.py#L138
-fileprivate var depthCoefficient: Float = 1.0
-fileprivate func roundBlockDepthUp(blockCount: Int) -> Int {
-    /// Multiply + round up the number of blocks based on global depth multiplier
-    var newFilterCount = depthCoefficient * Float(blockCount)
+fileprivate func resizeDepth(blockCount: Int, depth: Float) -> Int {
+    /// Multiply + round up the number of blocks based on depth multiplier
+    var newFilterCount = depth * Float(blockCount)
     newFilterCount.round(.up)
     return Int(newFilterCount)
 }
 
-fileprivate var widthCoefficient: Float = 1.0
-fileprivate func roundFilterCountDown(filter: Int, depthDivisor: Float = 8.0) -> Int {
-    /// Multiply + round down the number of filters based on global width multiplier
-    let filterMult = Float(filter) * widthCoefficient
-    let filterAdd = Float(filterMult) + (depthDivisor / 2.0)
-    var div = filterAdd / depthDivisor
+fileprivate func makeDivisible(filter: Int, width: Float, divisor: Float = 8.0) -> Int {
+    /// Return a filter multiplied by width, rounded down and evenly divisible by the divisor
+    let filterMult = Float(filter) * width
+    let filterAdd = Float(filterMult) + (divisor / 2.0)
+    var div = filterAdd / divisor
     div.round(.down)
-    div = div * Float(depthDivisor)
+    div = div * Float(divisor)
     var newFilterCount = max(1, Int(div))
     if newFilterCount < Int(0.9 * Float(filter)) {
-        newFilterCount += Int(depthDivisor)
+        newFilterCount += Int(divisor)
     }
     return Int(newFilterCount)
 }
 
-fileprivate func roundFilterPair(filters: (Int, Int)) -> (Int, Int) {
-    return (roundFilterCountDown(filter: filters.0), roundFilterCountDown(filter: filters.1))
+fileprivate func roundFilterPair(filters: (Int, Int), width: Float) -> (Int, Int) {
+    return (makeDivisible(filter: filters.0, width: width),
+            makeDivisible(filter: filters.1, width: width))
 }
 
 public struct InitialMBConvBlock: Layer {
@@ -59,19 +58,19 @@ public struct InitialMBConvBlock: Layer {
     public var conv2: Conv2D<Float>
     public var batchNormConv2: BatchNorm<Float>
 
-    public init(filters: (Int, Int)) {
-        let filterMult = roundFilterPair(filters: filters)
+    public init(filters: (Int, Int), width: Float) {
+        let filterMult = roundFilterPair(filters: filters, width: width)
         self.hiddenDimension = filterMult.0
         dConv = DepthwiseConv2D<Float>(
             filterShape: (3, 3, filterMult.0, 1),
             strides: (1, 1),
             padding: .same)
         seReduceConv = Conv2D<Float>(
-            filterShape: (1, 1, filterMult.0, roundFilterCountDown(filter: 8)),
+            filterShape: (1, 1, filterMult.0, makeDivisible(filter: 8, width: width)),
             strides: (1, 1),
             padding: .same)
         seExpandConv = Conv2D<Float>(
-            filterShape: (1, 1, roundFilterCountDown(filter: 8), filterMult.0),
+            filterShape: (1, 1, makeDivisible(filter: 8, width: width), filterMult.0),
             strides: (1, 1),
             padding: .same)
         conv2 = Conv2D<Float>(
@@ -111,6 +110,7 @@ public struct MBConvBlock: Layer {
 
     public init(
         filters: (Int, Int),
+        width: Float,
         depthMultiplier: Int = 6,
         strides: (Int, Int) = (1, 1),
         kernel: (Int, Int) = (3, 3)
@@ -118,7 +118,7 @@ public struct MBConvBlock: Layer {
         self.strides = strides
         self.addResLayer = filters.0 == filters.1 && strides == (1, 1)
 
-        let filterMult = roundFilterPair(filters: filters)
+        let filterMult = roundFilterPair(filters: filters, width: width)
         self.hiddenDimension = filterMult.0 * depthMultiplier
         let reducedDimension = max(1, Int(filterMult.0 / 4))
         conv1 = Conv2D<Float>(
@@ -174,15 +174,17 @@ public struct MBConvBlockStack: Layer {
 
     public init(
         filters: (Int, Int),
+        width: Float,
         initialStrides: (Int, Int) = (2, 2),
         kernel: (Int, Int) = (3, 3),
-        blockCount: Int
+        blockCount: Int,
+        depth: Float
     ) {
-        let blockMult = roundBlockDepthUp(blockCount: blockCount)
-        self.blocks = [MBConvBlock(filters: (filters.0, filters.1),
+        let blockMult = resizeDepth(blockCount: blockCount, depth: depth)
+        self.blocks = [MBConvBlock(filters: (filters.0, filters.1), width: width,
             strides: initialStrides, kernel: kernel)]
         for _ in 1..<blockMult {
-            self.blocks.append(MBConvBlock(filters: (filters.1, filters.1), kernel: kernel))
+            self.blocks.append(MBConvBlock(filters: (filters.1, filters.1), width: width, kernel: kernel))
         }
     }
 
@@ -220,38 +222,33 @@ public struct EfficientNet: Layer {
         resolution: Int = 224,
         dropout: Double = 0.2
     ) {
-        depthCoefficient = depth
-        widthCoefficient = width
-
         inputConv = Conv2D<Float>(
-            filterShape: (3, 3, 3, roundFilterCountDown(filter: 32)),
+            filterShape: (3, 3, 3, makeDivisible(filter: 32, width: width)),
             strides: (2, 2),
             padding: .same)
-        inputConvBatchNorm = BatchNorm(featureCount: roundFilterCountDown(filter: 32))
+        inputConvBatchNorm = BatchNorm(featureCount: makeDivisible(filter: 32, width: width))
 
-        // global filter resizing happens at block layer
-        initialMBConv = InitialMBConvBlock(filters: (32, 16))
+        initialMBConv = InitialMBConvBlock(filters: (32, 16), width: width)
 
-        // global block resizing happens at stack layer
-        residualBlockStack1 = MBConvBlockStack(filters: (16, 24), blockCount: 2)
-        residualBlockStack2 = MBConvBlockStack(filters: (24, 40), kernel: (5, 5),
-            blockCount: 2)
-        residualBlockStack3 = MBConvBlockStack(filters: (40, 80), blockCount: 3)
-        residualBlockStack4 = MBConvBlockStack(filters: (80, 112), initialStrides: (1, 1),
-            kernel: (5, 5), blockCount: 3)
-        residualBlockStack5 = MBConvBlockStack(filters: (112, 192), kernel: (5, 5),
-            blockCount: 4)
-        residualBlockStack6 = MBConvBlockStack(filters: (192, 320), initialStrides: (1, 1),
-            blockCount: 1)
+        residualBlockStack1 = MBConvBlockStack(filters: (16, 24), width: width, blockCount: 2, depth: depth)
+        residualBlockStack2 = MBConvBlockStack(filters: (24, 40), width: width, kernel: (5, 5),
+            blockCount: 2, depth: depth)
+        residualBlockStack3 = MBConvBlockStack(filters: (40, 80), width: width, blockCount: 3, depth: depth)
+        residualBlockStack4 = MBConvBlockStack(filters: (80, 112), width: width, initialStrides: (1, 1),
+            kernel: (5, 5), blockCount: 3, depth: depth)
+        residualBlockStack5 = MBConvBlockStack(filters: (112, 192), width: width, kernel: (5, 5),
+            blockCount: 4, depth: depth)
+        residualBlockStack6 = MBConvBlockStack(filters: (192, 320), width: width, initialStrides: (1, 1),
+            blockCount: 1, depth: depth)
 
         outputConv = Conv2D<Float>(
-            filterShape: (1, 1, roundFilterCountDown(filter: 320), roundFilterCountDown(filter: 1280)),
+            filterShape: (1, 1, makeDivisible(filter: 320, width: width), makeDivisible(filter: 1280, width: width)),
             strides: (1, 1),
             padding: .same)
-        outputConvBatchNorm = BatchNorm(featureCount: roundFilterCountDown(filter: 1280))
+        outputConvBatchNorm = BatchNorm(featureCount: makeDivisible(filter: 1280, width: width))
 
         dropoutProb = Dropout<Float>(probability: dropout)
-        outputClassifier = Dense(inputSize: roundFilterCountDown(filter: 1280), outputSize: classCount, activation: softmax)
+        outputClassifier = Dense(inputSize: makeDivisible(filter: 1280, width: width), outputSize: classCount, activation: softmax)
     }
 
     @differentiable
